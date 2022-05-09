4e52747a02d7201591b0fc28f05f09eec3dc6245
murryf <72839972+murryf@users.noreply.github.com>
2022-05-09 13:34:19 -0700

    Initial wire frame
    Outline of the Spring application

---
+++ src/test/java/dev/nexus/Potlukk/PotlukkApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nexus.Potlukk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotlukkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
