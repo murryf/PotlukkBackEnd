79a7c53381c057b4b917f64fe57ffa7a6520bda0
Lamech Israel <lamech639@revature.net>
2022-05-10 15:47:28 -0700

    PotluckRepo Tests complete; Changes to Entity; Changes to Service and Impl; Edited Item to allow testing. (Need User creation for further testing)

--- src/main/java/dev/nexus/services/PotluckService.java
+++ src/main/java/dev/nexus/services/PotluckService.java
@@ -7,9 +7,12 @@ import java.util.List;
 public interface PotluckService {
 
     Potluck createPotluck(Potluck potluck);
+
     List<Potluck> getAllPotlucks();
+//    List<Potluck> getPotlucksByCreator(int creatorId);
     Potluck getPotluckById(int id);
-    Potluck updatePotluckTime(int id);
-    Potluck deletePotluckById(int id);
+
+    Potluck updatePotluckTime(int id, double date);
+    boolean deletePotluckById(int id);
 
 }
