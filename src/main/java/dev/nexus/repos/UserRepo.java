deca64257c945f83e1bf01d525103abe2f1cc011
k-n-terry <khenan.n.terry@gmail.com>
2022-05-11 10:05:34 -0400

    Added UserRepo

---
+++ src/main/java/dev/nexus/repos/UserRepo.java
@@ -0,0 +1,18 @@
+package dev.nexus.repos;
+
+import dev.nexus.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @Component marks class as a Bean -> ComponentScan adds Bean to Application Context
+ * @Repository marks classes, which directly access Database
+ */
+@Component
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer>{
+	List<User> getUserById(int id);
+}
