32d15d44e684f371fec9d4cfce55b1e61623d3c5
Paul M <63323717+paulm233@users.noreply.github.com>
2022-05-12 14:45:41 -0400

    Added UserController

---
+++ src/main/java/dev/nexus/controllers/UserController.java
@@ -0,0 +1,55 @@
+package dev.nexus.controllers;
+
+
+import dev.nexus.entities.User;
+import dev.nexus.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Component
+@RestController
+@CrossOrigin("*")
+public class UserController {
+
+    @Autowired
+    public UserService userService;
+
+
+    @PostMapping("/users")
+    @ResponseBody
+    public User createNewUser(@RequestBody User user){
+        return this.userService.registerUser(user);
+    }
+
+
+
+    @GetMapping("/users")
+    @ResponseBody
+    public List<User> retrieveAllUsers(){
+        List<User> users = this.userService.getAllUsers();
+        return users;
+    }
+
+
+    @GetMapping("/users/{id}")
+    @ResponseBody
+    public User retrieveUserById(@PathVariable int id){
+        User user = this.userService.getUserByID(id);
+        return user;
+    }
+
+
+    @DeleteMapping("/users/{id}")
+    @ResponseBody
+    public boolean deleteUserById(@PathVariable int id){
+        this.userService.deleteUser(id);
+        return true;
+    }
+
+
+
+
+}
