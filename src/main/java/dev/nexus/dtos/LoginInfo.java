0bb0bb8f96e733dbcda23e074409ebedf469f1c7
Paul M <63323717+paulm233@users.noreply.github.com>
2022-05-16 16:18:54 -0400

    Add files via upload
    Login!!

---
+++ src/main/java/dev/nexus/dtos/LoginInfo.java
@@ -0,0 +1,33 @@
+package dev.nexus.dtos;
+
+public class LoginInfo {
+
+    private String userName;
+    private String password;
+
+    public LoginInfo(){
+
+    }
+
+    public LoginInfo(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/dev/nexus/dtos/UserInfo.java
@@ -0,0 +1,32 @@
+package dev.nexus.dtos;
+
+public class UserInfo {
+
+    private int userID;
+    private String userName;
+
+    public UserInfo(){
+
+    }
+
+    public UserInfo(int userID, String userName) {
+        this.userID = userID;
+        this.userName = userName;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
